7aab6965774c9b5482d71232d90e1e851e5590cf
Valentin Vogel <18506336+valentin-vogel@users.noreply.github.com>
2026-08-02 01:16:45 +0200

    chore: shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,20 @@
 import type { Metadata } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
+import { DM_Sans, Source_Serif_4, JetBrains_Mono } from "next/font/google"
 import "./globals.css"
+import { cn } from "@/lib/utils"
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const dmSans = DM_Sans({
+  variable: "--font-dm-sans",
   subsets: ["latin"],
 })
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const sourceSerif4 = Source_Serif_4({
+  variable: "--font-source-serif-4",
+  subsets: ["latin"],
+})
+
+const jetBrainsMono = JetBrains_Mono({
+  variable: "--font-jet-brains-mono",
   subsets: ["latin"],
 })
 
@@ -23,11 +29,19 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en">
+      <body
+        className={cn(
+          dmSans.variable,
+          sourceSerif4.variable,
+          jetBrainsMono.variable,
+          "h-full",
+          "antialiased",
+          "font-sans",
+        )}
+      >
+        {children}
+      </body>
     </html>
   )
 }
