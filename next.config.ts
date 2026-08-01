5aa8e3288fdd15bfc486ebcf1aad9ba820d29c04
Valentin Vogel <18506336+valentin-vogel@users.noreply.github.com>
2026-08-02 01:46:33 +0200

    chore: init payloadcms

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
+import { withPayload } from "@payloadcms/next/withPayload"
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {/* config options here */}
 
-export default nextConfig
+export default withPayload(nextConfig)
